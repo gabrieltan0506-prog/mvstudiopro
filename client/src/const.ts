6f9d59a6a445f7ed1cc1d8aedb7e530f45a0e690
gabrieltan0506-prog <gabrieltan0506@gmail.com>
2026-02-23 02:29:06 +0800

    Update const.ts

--- client/src/const.ts
+++ client/src/const.ts
@@ -7,11 +7,20 @@ export const getLoginUrl = () => {
   const redirectUri = `${window.location.origin}/api/oauth/callback`;
   const state = btoa(redirectUri);
 
+  
+  if (!oauthPortalUrl || !oauthPortalUrl.startsWith("http")) {
+    return `${window.location.origin}/api/auth/google/start`;
+  }
+
+ 
   const url = new URL(`${oauthPortalUrl}/app-auth`);
-  url.searchParams.set("appId", appId);
+  url.searchParams.set("appId", appId ?? "");
   url.searchParams.set("redirectUri", redirectUri);
   url.searchParams.set("state", state);
   url.searchParams.set("type", "signIn");
+
+  return url.toString();
+};
 
   return url.toString();
 };
